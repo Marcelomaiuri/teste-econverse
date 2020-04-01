626db7f8df055158292c8c48853820da62a912e7
Marcelomaiuri <marcelobonfimmaiuri@gmail.com>
2020-03-31 23:55:25 -0300

    feat: melhores marcas com slider

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
         dots: true,
         autoplay:true
     });
+
+    $('.melhores-marcas-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay:true
+    });
 })
