2554a71e343c6fb1536a075ab1144799dc09a7f4
Marcelomaiuri <marcelobonfimmaiuri@gmail.com>
2020-04-01 16:18:42 -0300

    feat: capturando melhores produtos por json

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
+
     $('#main-banner').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         dots: true,
-        autoplay:true
+        autoplay: true
     });
 
     $('.melhores-marcas-slider').slick({
-        infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
         dots: false,
-        autoplay:true
+        autoplay: true
     });
 
     $('.comentarios-row').slick({
@@ -23,6 +23,45 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         dots: false,
-        autoplay:true
+        autoplay: true
     });
-})
+
+    $.ajax({
+        url: 'http://localhost:8080/json/products-ninjasom.json',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'contentType': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function (response) {
+            console.log(response.length)
+            for (var i = 0; i < response.length; i++) {
+                var imgProduto = response[i].items[0].images[0].imageUrl,
+                    nomeProduto = response[i].productName,
+                    descricaoProduto = response[i].brand,
+                    precoProduto = response[i].items[0].sellers[0].commertialOffer.Price
+
+                var htmlPrateleira = `<div class="prateleira-col">
+                    <img src="${imgProduto}" />
+                    <h3 class="prateleira-titulo">${nomeProduto}</h3>
+                    <p class="prateleira-descricao">${descricaoProduto}</p>
+                    <p class="prateleira-preco">${precoProduto.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                </div>`;
+
+                $(".prateleira-row").append(htmlPrateleira);
+
+            }
+
+            $('.prateleira-row').slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+                autoplay: true
+            });
+
+
+        }
+    });
+});
