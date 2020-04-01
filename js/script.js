b564a7dd46c158cc1a265780e30d29413d9acf12
Marcelomaiuri <marcelobonfimmaiuri@gmail.com>
2020-04-01 01:49:44 -0300

    feat:adicionando comentarios

--- js/script.js
+++ js/script.js
@@ -16,4 +16,13 @@ $(document).ready(function () {
         dots: false,
         autoplay:true
     });
+
+    $('.comentarios-row').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay:true
+    });
 })
